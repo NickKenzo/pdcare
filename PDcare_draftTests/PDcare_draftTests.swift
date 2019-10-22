d39cd28202a302e92705077708b34494d1cc21c2
Tianhao Li <tianhaol@asb9700-e01.csil.sfu.ca>
2019-10-22 11:08:59 -0700

    Initial Commit

---
+++ PDcare_draftTests/PDcare_draftTests.swift
@@ -0,0 +1,34 @@
+//
+//  PDcare_draftTests.swift
+//  PDcare_draftTests
+//
+//  Created by Tianhao Li on 10/22/19.
+//  Copyright © 2019 Group_14. All rights reserved.
+//
+
+import XCTest
+@testable import PDcare_draft
+
+class PDcare_draftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
