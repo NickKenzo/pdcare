aa287e45e4197d647808392f9992653678516c9a
nskinner <nskinner@asb9700-e03.csil.sfu.ca>
2019-10-25 16:13:05 -0700

    Added memory game base file

--- PDCare/GamesMenuVC.swift
+++ PDCare/GamesMenuVC.swift
@@ -19,7 +19,10 @@ class GamesMenuVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func openMemoryGame(_ sender: Any) {
+        performSegue(withIdentifier: "memorySegue", sender: self)
+    }
+    
     /*
     // MARK: - Navigation
 
