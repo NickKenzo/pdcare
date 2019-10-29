3bc9052ed9c4480bf313ed28a9a5929ada69e29f
Kyle Smolko <ksmolko2@gmail.com>
2019-10-28 17:08:18 -0700

    Modified balance game scene to include a ball and wall objects

--- PDCare/BalanceGameScene.swift
+++ PDCare/BalanceGameScene.swift
@@ -7,16 +7,34 @@
 //
 
 import SpriteKit
-import GameplayKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate{
+    
+    let manager = CMMotionManager()
+    var ball = SKSpriteNode()
+    var mazeEnd = SKSpriteNode()
     
     override func didMove(to view: SKView) {
-
+        
+        
+        self.physicsWorld.contactDelegate = self
+        
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        
+        manager.startAccelerometerUpdates()
+        manager.accelerometerUpdateInterval = 0.1
+        manager.startAccelerometerUpdates(to: OperationQueue.main){
+            (data, error) in
+            
+            self.physicsWorld.gravity = CGVector.init(dx: CGFloat((data?.acceleration.x)!) * 10, dy: CGFloat((data?.acceleration.y)!) * 10)
+            
+        }
+        
     }
     
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+    
+    override func update(_ currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
     }
 }
