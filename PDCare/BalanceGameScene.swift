ea80aa19ffd6807283537678182e72ec65467050
Kyle Smolko <ksmolko2@gmail.com>
2019-11-03 19:25:32 -0800

    Added another level for balance game and a framework to create more levels from here on

--- PDCare/BalanceGameScene.swift
+++ PDCare/BalanceGameScene.swift
@@ -17,11 +17,15 @@ class BalanceGameScene: SKScene, SKPhysicsContactDelegate{
                SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),
                SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),
                SKSpriteNode()]
-    var goal = SKSpriteNode()
+    var goal = [SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),
+                SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),
+                SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),
+                SKSpriteNode()]
     var score = 1000000
     var resetPos = false
-    var startingPos=CGPoint(x: 0, y: 0)
+    var startingPos = CGPoint(x: 0, y: 0)
     var scoreDisplay = SKLabelNode();
+    var mapNum = 1
     
     override func didMove(to view: SKView) {
         
@@ -30,73 +34,59 @@ class BalanceGameScene: SKScene, SKPhysicsContactDelegate{
         // Initialize sprites
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         map[0] = self.childNode(withName: "map0") as! SKSpriteNode
-        goal = self.childNode(withName: "goal") as! SKSpriteNode
+        goal[0] = self.childNode(withName: "goal0") as! SKSpriteNode
+        map[1] = self.childNode(withName: "map1") as! SKSpriteNode
+        goal[1] = self.childNode(withName: "goal1") as! SKSpriteNode
         
-        startingPos=ball.position
+        for i in 1...map.count {
+            if i != mapNum + 1 {
+                map[i - 1].isHidden = true
+                goal[i - 1].isHidden = true
+            }
+        }
         
-        // Initialize physics bodies
-        //ball.physicsBody = SKPhysicsBody(circleOfRadius: 32)
-        
-        // Initialize masks
-        ball.physicsBody?.contactTestBitMask = 1
-        ball.physicsBody?.categoryBitMask = 1
-        map[0].physicsBody?.contactTestBitMask = 1
-        map[0].physicsBody?.categoryBitMask = 1
-        goal.physicsBody?.contactTestBitMask = 1
-        goal.physicsBody?.categoryBitMask = 1
+        startingPos = ball.position
         
         manager.startAccelerometerUpdates()
         manager.accelerometerUpdateInterval = 0.1
         manager.startAccelerometerUpdates(to: OperationQueue.main){
             (data, error) in
-            
-            /**** Currently physicsWorld.gravity does not override settings in sks file and we need to figure out why ****/
-            /**** Works if you set it in the func update, see below ****/
-            //self.physicsWorld.gravity = CGVector(dx: CGFloat((data?.acceleration.x)!) * 0, dy: CGFloat((data?.acceleration.y)!) * 0)
-            //self.physicsWorld.gravity = CGVector(dx: CGFloat(0), dy: CGFloat(0))
-            
         }
     }
     
     func didBegin(_ contact: SKPhysicsContact) {
         // Called when one object ends contact with another
-        if (contact.bodyB.node?.name == "wall") || (contact.bodyA.node?.name == "wall") {
+        if (contact.bodyB.node?.parent?.name == map[mapNum].name) || (contact.bodyA.node?.parent?.name == map[mapNum].name) {
             score -= 5000
-            
-            /*** Ball position below does not get set and we need to figure out why ***/
-            //ball.position = CGPoint(x: 256.67, y: -640)
             resetPos = true
-            
         }
         
-        if contact.bodyA.node?.name == "goal" || contact.bodyB.node?.name == "goal" {
+        if contact.bodyA.node?.name == goal[mapNum].name || contact.bodyB.node?.name == goal[mapNum].name {
             if let view = self.view as! SKView? {
-            if let scene = SKScene(fileNamed: "BalanceGameOverScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-                scoreDisplay = scene.childNode(withName: "scoreDisplay") as! SKLabelNode;
-                scoreDisplay.text = "Score:" + String(score);
-                scoreDisplay.fontSize = 65;
+                if let scene = SKScene(fileNamed: "BalanceGameOverScene") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view.presentScene(scene)
+                    scoreDisplay = scene.childNode(withName: "scoreDisplay") as! SKLabelNode;
+                    scoreDisplay.text = "Score:" + String(score);
+                    scoreDisplay.fontSize = 65;
+                }
             }
-        }
         }
     }
     
     override func update(_ currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        ball.physicsBody?.contactTestBitMask = 1
+        ball.physicsBody?.categoryBitMask = 1
         score -= 100;
         scoreDisplay = self.childNode(withName: "scoreDisplay") as! SKLabelNode;
         scoreDisplay.text = "Score:" + String(score);
         scoreDisplay.fontSize = 65;
         
-        //let accelerometerData = manager.accelerometerData
         self.physicsWorld.gravity = CGVector(dx: ((manager.accelerometerData?.acceleration.x ?? 0) * 5), dy: ((manager.accelerometerData?.acceleration.y ?? 0) * 5))
- 
-        //this works
-        //self.physicsWorld.gravity = CGVector(dx: 0, dy: -1)
         
         if (resetPos){
             ball.position = startingPos
