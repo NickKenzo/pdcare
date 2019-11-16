9c679c3ee2034d6d31dab9c5c56ced5d0165888a
Daniel Wan <penguinsstar@gmail.com>
2019-11-15 23:51:32 -0800

    Added persistent settings

--- PDCare/Menu/SettingsVC.swift
+++ PDCare/Menu/SettingsVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import AVFoundation
+import Foundation
 
 class SettingsVC: UITableViewController{
     
@@ -19,9 +19,18 @@ class SettingsVC: UITableViewController{
     @IBOutlet weak var notificationSwitch: UISwitch!
     @IBOutlet weak var offlineSwitch: UISwitch!
     
-    @IBOutlet weak var audioLabel: UILabel!
-    @IBOutlet weak var notificationLabel: UILabel!
-    @IBOutlet weak var offlineLabel: UILabel!
+    
+    let defaults = UserDefaults.standard
+       
+    @IBAction func audioSwitch(_ sender: UISwitch) {
+        defaults.set(audioSwitch.isOn, forKey: "audio")
+    }
+    @IBAction func notificationSwitch(_ sender: UISwitch) {
+        defaults.set(notificationSwitch.isOn, forKey: "notification")
+    }
+    @IBAction func offlineSwitch(_ sender: UISwitch) {
+        defaults.set(offlineSwitch.isOn, forKey: "offline")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +38,27 @@ class SettingsVC: UITableViewController{
         audioSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
         notificationSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
         offlineSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
+        
+        
+        if !(exist(key: "audio")){
+            defaults.set(true, forKey: "audio")
+            defaults.set(true, forKey: "notification")
+            defaults.set(false, forKey: "offline")
+        }
+        audioSwitch.isOn = defaults.bool(forKey: "audio")
+        notificationSwitch.isOn = defaults.bool(forKey: "notification")
+        offlineSwitch.isOn = defaults.bool(forKey: "offline")
+        
+        
     }
 }
+
+func exist(key: String) -> Bool {
+    return UserDefaults.standard.object(forKey: key) != nil
+}
+
+
+
     
     
 
