2b11947bb2f8241b76285032384e87ea9fbfed3a
Daniel Wan <penguinsstar@gmail.com>
2019-11-28 21:27:01 -0800

    UI Progress on Login page

--- PDCare/Menu/LoginVC.swift
+++ PDCare/Menu/LoginVC.swift
@@ -19,13 +19,83 @@ import UIKit
 
 class LoginVC: UIViewController {
 
-    @IBAction func openMainMenu(_ sender: Any) {
-        performSegue(withIdentifier: "MainMenuSegue", sender: self)
+    @IBAction func openSignUpScreen(_ sender: Any) {
+        performSegue(withIdentifier: "LoginScreenSegue", sender: self)
     }
+    
+    @IBAction func openMainMenu(_ sender: Any) {
+        performSegue(withIdentifier: "MainMenuSegue", sender: self)     
+    }
+    
+    @IBOutlet weak var TitleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        self.hideKeyboardWhenTappedAround()
+        
+        let strokeTextAttributes = [
+            NSAttributedString.Key.strokeColor : UIColor.red,
+            NSAttributedString.Key.foregroundColor : UIColor.white,
+            NSAttributedString.Key.strokeWidth : -4.0,
+            NSAttributedString.Key.font : UIFont.boldSystemFont(ofSize: 30)]
+            as [NSAttributedString.Key : Any]
+        //Making outline here
+        TitleLabel.attributedText = NSMutableAttributedString(string: "Welcome to P.D. Care", attributes: strokeTextAttributes)
+        
+        
+        
+    }    
+    
     
 }
+
+//Adds UI button color, width, radius attributes to storyboard editor
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+//dismiss keyboard when tapping anywhere
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
+
