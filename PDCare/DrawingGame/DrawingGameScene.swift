37b601278c2a112a0e381e93d1c352f620db1c8f
Jacky Lin <jla477@sfu.ca>
2019-12-02 00:33:09 -0800

    Done with DrawingGame

--- PDCare/DrawingGame/DrawingGameScene.swift
+++ PDCare/DrawingGame/DrawingGameScene.swift
@@ -9,17 +9,14 @@
 import SpriteKit
 import CoreMotion
 
-//protocol GameOverDelegate {
-//
-//    func goback()
-//}
+
+
+var drawingMap = [SKSpriteNode(), SKSpriteNode(), SKSpriteNode(),SKSpriteNode()]
+
 
 class DrawingGameScene: SKScene, SKPhysicsContactDelegate{
     
     let manager = CMMotionManager()
-    var score = 100
-    var map = [SKSpriteNode(), SKSpriteNode(), SKSpriteNode()]
-    var game_over : GameOverDelegate?
     var mapNum = 0
     var scoreDisplay = SKLabelNode()
     override func didMove(to view: SKView) {
@@ -28,52 +25,41 @@ class DrawingGameScene: SKScene, SKPhysicsContactDelegate{
         mapNum = Int.random(in: 0 ..< 2)
         
         //Initialize sprites
-        map[0] = self.childNode(withName: "map0") as! SKSpriteNode
-        map[1] = self.childNode(withName: "map1") as! SKSpriteNode
-        for i in 1...map.count {
+        drawingMap[0] = self.childNode(withName: "map0") as! SKSpriteNode
+        drawingMap[1] = self.childNode(withName: "map1") as! SKSpriteNode
+        for i in 1...drawingMap.count {
             if i != mapNum + 1 {
-                map[i - 1].isHidden = true                
+                drawingMap[i - 1].isHidden = true
             }
         }
-            
-    }
+//        let temp = DrawingGameVC()
+//        if mapNum == 0{
+//            if temp.tryAgain == true{
+//
+//                drawingMap[1].isHidden = true
+//            }
+//            if temp.nextGame == true{
+//                drawingMap [0].isHidden = true
+//            }
+//        }
+//        if mapNum == 1{
+//            if temp.tryAgain == true{
+//
+//                drawingMap[0].isHidden = true
+//            }
+//            if temp.nextGame == true{
+//                drawingMap [1].isHidden = true
+//            }
+//        }
+        
+        
 
-    
-    func gameOver() {
-        if let view = self.view {
-            if let scene = DrawingGameOverScene(fileNamed: "DrawingGameOverScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                scene.score=score
-                scene.game_over=game_over
-                // Present the scene
-                view.presentScene(scene, transition: SKTransition.crossFade(withDuration: 1))
-                
-            }
-        }
     }
     
     
-//    override func update(_ currentTime: CFTimeInterval) {
-//        /* Called before each frame is rendered */
-//        if score <= 0 {
-//            gameOver()
-//        }
-//
-//        score -= 500
-//        scoreDisplay = self.childNode(withName: "scoreDisplay") as! SKLabelNode
-//        if score <= 0 {
-//            scoreDisplay.text = "Score: 0"
-//        }
-//        else {
-//            scoreDisplay.text = "Score: " + String(score)
-//        }
-//        scoreDisplay.fontSize = 65;
-//
-//
-//    }
+
 }
 
-    
+
 
 
