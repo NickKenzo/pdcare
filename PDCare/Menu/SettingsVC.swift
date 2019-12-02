2829bb2852b196892117864db6771f77149a6f7f
Daniel Wan <penguinsstar@gmail.com>
2019-12-02 14:14:02 -0800

    Deleted Stay Offline from Settins

--- PDCare/Menu/SettingsVC.swift
+++ PDCare/Menu/SettingsVC.swift
@@ -25,7 +25,6 @@ class SettingsVC: UITableViewController{
     
     @IBOutlet weak var audioSwitch: UISwitch!
     @IBOutlet weak var notificationSwitch: UISwitch!
-    @IBOutlet weak var offlineSwitch: UISwitch!
     
     
     // Set persistent user settings
@@ -73,27 +72,21 @@ class SettingsVC: UITableViewController{
             }
         }
     }
-    
-    @IBAction func changeOfflineSetting(_ sender: UISwitch) {
-        defaults.set(offlineSwitch.isOn, forKey: "offline")
-    }
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         audioSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
         notificationSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
-        offlineSwitch.transform = CGAffineTransform(scaleX: 2, y: 2)
         
         
         if !(exist(key: "audio")){
             defaults.set(true, forKey: "audio")
             defaults.set(false, forKey: "notification")
-            defaults.set(false, forKey: "offline")
         }
         audioSwitch.isOn = defaults.bool(forKey: "audio")
         notificationSwitch.isOn = defaults.bool(forKey: "notification")
-        offlineSwitch.isOn = defaults.bool(forKey: "offline")
     }
 }
 
